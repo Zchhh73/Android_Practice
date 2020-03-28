c02f9929bd93166dfbcab5515f0bbcae3f265396
zchhh <13671292697@163.com>
2020-03-28 21:29:18 +0800

    第二次提交--登录模式

---
+++ LaunchMode/app/src/main/java/com/zchhh/launchmode/MainActivity.java
@@ -0,0 +1,28 @@
+package com.zchhh.launchmode;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = (TextView)findViewById(R.id.process_tv);
+        Button button = (Button)findViewById(R.id.process_btn);
+
+        textView.setText(this.toString());
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ LaunchMode/app/src/main/java/com/zchhh/launchmode/SecondActivity.java
@@ -0,0 +1,27 @@
+package com.zchhh.launchmode;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        TextView textView = (TextView)findViewById(R.id.second_tv);
+        textView.setText(this.toString());
+        Button btn2 = (Button)findViewById(R.id.second_btn);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
